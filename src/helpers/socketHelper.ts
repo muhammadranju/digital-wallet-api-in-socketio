ac0b1ce96272d545d769afdc27b4b2622dfd3f54
Muhammad Ranju <mdranju23@gmail.com>
2025-08-13 03:35:40 +0600

    feat: added add-money and withdraw socket work

--- src/helpers/socketHelper.ts
+++ src/helpers/socketHelper.ts
@@ -1,17 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import colors from 'colors';
 import { Server, Socket } from 'socket.io';
+import { Wallet } from '../app/modules/wallets/wallets.model';
 import { logger } from '../shared/logger';
 
-type AuthenticatedSocket = {
+type AuthenticatedSocket = Socket & {
   user?: { id: string; role: string };
-} & Socket;
+};
+
 const socket = (io: Server) => {
   io.on('connection', (socket: AuthenticatedSocket) => {
     logger.info(colors.blue('A user connected'));
+    const userSid = socket.id;
 
-    //disconnect
+    const handleWalletAction = async (
+      action: string,
+      notifyEvent: string,
+      data: any
+    ) => {
+      try {
+        socket.user = { id: data.userId, role: data.role };
+
+        const wallet = await Wallet.findById(socket.user?.id);
+        if (!wallet) {
+          return socket.emit('error', { message: 'Wallet not found' });
+        }
+        if (wallet.isBlocked) {
+          return socket.emit('error', { message: 'This wallet is blocked' });
+        }
+
+        socket.emit(notifyEvent, { userSid, ...data });
+      } catch (err) {
+        logger.error(err);
+        socket.emit('error', { message: 'Internal server error' });
+      }
+    };
+
+    socket.on('add-money', data =>
+      handleWalletAction('add-money', 'add-money-notify', data)
+    );
+
+    socket.on('withdraw', data =>
+      handleWalletAction('withdraw', 'withdraw-notify', data)
+    );
+
     socket.on('disconnect', () => {
-      logger.info(colors.red('A user disconnect'));
+      logger.info(colors.red('A user disconnected'));
     });
   });
 };
